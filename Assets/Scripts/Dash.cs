b771586c04423968ce6153715bea68a55da5f850
DrakeSingh <drakesingh1@gmail.com>
2019-05-26 18:49:38 +0530

    Initial commit

---
+++ Assets/Scripts/Dash.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+    public ParticleSystem partical;
+    public ParticleSystem burst;
+
+    public SkinnedMeshRenderer joints; 
+    public SkinnedMeshRenderer surface;
+
+    private MovementController speed;
+
+    public bool playing;
+
+    void Start()
+    {
+        speed = GetComponent<MovementController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl) && speed.isWalking)
+            playing = true;
+
+        else
+            playing = false;
+
+
+
+        if (playing)
+        {
+            if (!partical.isPlaying)
+            {
+                burst.Play();
+                partical.Play();
+                joints.enabled = false;
+                surface.enabled = false;
+                speed.gravity = 0.2f;
+                speed.movementSpeed = 20.0f;
+                speed.jumpSpeed = 15.0f;
+            }
+        }
+        else
+        {
+            if (partical.isPlaying)
+            {
+                burst.Play();
+                partical.Stop();
+                joints.enabled = true;
+                surface.enabled = true;
+                speed.movementSpeed = 10.0f;
+                speed.gravity = 0.5f;
+                speed.jumpSpeed = 5.0f;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MovementController.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class MovementController : MonoBehaviour
+{
+    private float InputX, InputZ, Speed, verticalVelocity;
+
+    private Camera cam;
+    private CharacterController characterController;
+    private Animator anim;
+    private Rigidbody rb;
+
+    private Vector3 desiredMoveDirection;
+    public bool isWalking;
+    public float movementSpeed = 1f;
+    public float gravity = 0.5f;
+    public float jumpSpeed = 10.0f;
+
+    [SerializeField] float rotationSpeed = 0.3f;
+    [SerializeField] float allowRotation = 0.1f; 
+    [SerializeField] bool grounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+        cam = Camera.main;
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InputX = Input.GetAxis("Horizontal");
+        InputZ = Input.GetAxis("Vertical");
+
+        InputDecider();
+        MovementManager();
+    }
+
+
+    void InputDecider() 
+    {
+        Speed = new Vector2(InputX, InputZ).sqrMagnitude;
+
+        isWalking = Speed > 0;
+        anim.SetBool("isRunning", isWalking);
+
+        if (Speed > allowRotation)
+        {
+            RotationManager();
+        }
+        else
+        {
+            desiredMoveDirection = Vector3.zero;
+        }
+
+    }
+
+
+
+    void RotationManager()
+    {
+        var forward = cam.transform.forward;
+        var right = cam.transform.right;
+
+        forward.y = 0;
+        right.y = 0;
+
+        forward.Normalize();
+        right.Normalize();
+
+        desiredMoveDirection = forward * InputZ + right * InputX;
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(desiredMoveDirection), rotationSpeed);
+
+    }
+
+    void MovementManager()
+    {
+        Vector3 moveDirection = desiredMoveDirection * (movementSpeed * Time.deltaTime);
+        moveDirection = new Vector3(moveDirection.x, verticalVelocity, moveDirection.z);
+        characterController.Move(moveDirection); 
+
+        grounded = characterController.isGrounded;
+        if(grounded)
+        {
+            verticalVelocity = -gravity * Time.deltaTime;
+            anim.SetBool("isGrounded", grounded);
+            if (Input.GetButtonDown("Jump"))
+            {
+                anim.SetTrigger("Jump");
+                verticalVelocity = jumpSpeed * Time.deltaTime;
+            }
+        }
+        else
+        {
+            anim.SetBool("isGrounded", grounded);
+
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
+
+    }
+
+
+
+}
